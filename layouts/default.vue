eeeef35d6cb9313612a28a4aff5a29e2c3e02e1c
venkatsundaraj <venkateshsundarasan@gmail.com>
2025-05-17 18:29:12 +0530

    intro

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <AppHeader />
+    <h1>Hello World Layout</h1>
+    <slot />
+    <AppFooter />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
